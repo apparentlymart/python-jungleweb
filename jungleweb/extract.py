389c7195564a6b810a95430fec3743d46d69b572
Martin Atkins <mart@degeneration.co.uk>
2012-09-29 16:05:29 -0700

    Basic content extractor

---
+++ jungleweb/extract.py
@@ -0,0 +1,17 @@
+
+import opengraph
+from urlparse import urlparse
+from jungleweb.page import Page
+
+
+def fetch_and_extract(url):
+    data = opengraph.OpenGraph(url=url)
+    p = Page()
+    p.title = data.get("title", None)
+    p.description = data.get("description", None)
+    p.url = data.get("url", None)
+    p.image_url = data.get("image", None)
+    p.ogp_type = data.get("type", None)
+    p.fetch_url = data.get("_url", None)
+    p.site_name = data.get("site_name", None)
+    return p
